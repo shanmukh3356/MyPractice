986f5769afae7576668a587a918e4a1cb97b56d0
Shanmukhsaireddy Tamma <141006806+shanmukh3356@users.noreply.github.com>
2024-01-17 18:34:29 +0530

    commit4

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,7 +2,7 @@
 
 echo "Hi This is shanmukh"
 PERSON1=Hemanth
-PERSON2=SAI
+PERSON2=Sai
 echo "$PERSON1: Hi, $PERSON2 how are you"
 echo "$PERSON2: Hi $PERSON1 Good morning"
 echo "$PERSON1: You looking good $PERSON2"

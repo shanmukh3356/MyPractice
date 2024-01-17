04a6c02f13110b5b4d3ed43a2d69026680c5ed0f
Shanmukhsaireddy Tamma <141006806+shanmukh3356@users.noreply.github.com>
2024-01-17 18:51:42 +0530

    commit

---
+++ 03-sending-vars.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+echo "Hi This is shanmukh"
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hi, $PERSON2 how are you"
+echo "$PERSON2: Hi $PERSON1 Good morning"
+echo "$PERSON1: You looking good $PERSON2"
+echo "$PERSON2: You to looking good $PERSON1"
